5d1310b7463ab150a7799f422e07c8346c61d94c
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-02-16 18:31:14 +0530

    feat: view images instantly

--- copymemetoclipboard.py
+++ copymemetoclipboard.py
@@ -20,13 +20,8 @@ def getmemes(genre = "memes"):
         getmemes()
     else:
         im = Image.open(requests.get(json_response['url'], stream=True).raw)
+        im.show()
 
-        output = BytesIO()
-        im.convert("RGB").save(output, "BMP")
-        data = output.getvalue()[14:]
-        output.close()
-
-        send_to_clipboard(win32clipboard.CF_DIB, data)
 
 
 def send_to_clipboard(clip_type, data):
